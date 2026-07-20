83003607958a4c33e9252ed42c5dd2c171eeff43
indraplrg <indraplrg@gmail.com>
2026-07-20 22:49:13 +0800

    add click animation to the navbar button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router";
-import Button from "./Button";
+import { NavLink } from "react-router";
 import {
   EnvelopeIcon,
   FilesIcon,
@@ -14,15 +13,36 @@ import {
 const Navbar = () => {
   return (
     <div className="fixed right-3 bottom-3 left-4 z-20 border-2 bg-[#f5f5f7] px-4 py-2 shadow-[6px_6px_0px_0px_rgba(34,34,34,1)] md:static md:min-h-screen md:shadow-none">
-      <ul className="h-fullfont-bricolage-grotesque flex justify-around font-medium md:hidden">
+      <ul className="font-bricolage-grotesque flex h-full justify-around font-medium md:hidden">
         <li>
-          <Link to="/">Home</Link>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              `h-full w-full cursor-pointer transition-all ease-in-out hover:text-[#a1d12c] ${isActive ? "text-[#a1d12c]" : ""}`
+            }
+          >
+            Home
+          </NavLink>
         </li>
         <li>
-          <Link to="/projects">Project</Link>
+          <NavLink
+            to="/projects"
+            className={({ isActive }) =>
+              `cursor-pointerh-fullw-full transition-all ease-in-out hover:text-[#a1d12c] ${isActive ? "text-[#a1d12c]" : ""}`
+            }
+          >
+            Project
+          </NavLink>
         </li>
         <li className="pr-1">
-          <Link to="/contact">Contact</Link>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) =>
+              `cursor-pointerh-fullw-full transition-all ease-in-out hover:text-[#a1d12c] ${isActive ? "text-[#a1d12c]" : ""}`
+            }
+          >
+            Contact
+          </NavLink>
         </li>
       </ul>
       <h1 className="font-bebas-neue my-5 hidden text-6xl font-semibold capitalize md:block">
@@ -30,64 +50,64 @@ const Navbar = () => {
       </h1>
       <div className="hidden flex-col justify-between gap-3 md:flex">
         <div className="flex flex-col gap-3">
-          <Button classname="w-42 ">
-            <Link
-              className="text-md flex w-full items-center gap-x-4 border px-3 py-2 text-start font-semibold"
-              to="/"
-            >
-              <HouseIcon size={24} />
-              Home
-            </Link>
-          </Button>
-          <Button classname="w-42 ">
-            <Link
-              className="flex w-full gap-x-4 border px-3 py-2 text-start font-semibold"
-              to="/projects"
-            >
-              <FilesIcon size={24} />
-              Project
-            </Link>
-          </Button>
-          <Button classname="w-42 ">
-            <Link
-              className="flex w-full gap-x-4 border px-3 py-2 text-start font-semibold"
-              to="/skills"
-            >
-              <TerminalIcon size={24} />
-              Skills
-            </Link>
-          </Button>
-          <Button classname="w-42 ">
-            <Link
-              className="flex w-full gap-x-4 border px-3 py-2 text-start font-semibold"
-              to="/contact"
-            >
-              <EnvelopeIcon size={24} />
-              Contact
-            </Link>
-          </Button>
+          <NavLink
+            className={({ isActive }) =>
+              `font-bricolage-grotesque hover:translate-y-1px flex w-42 cursor-pointer gap-2 gap-x-4 border-2 border-black px-3 py-2 text-start font-semibold shadow-[4px_4px_0px_0px_rgba(34,34,34,1)] transition-all hover:translate-x-1 hover:bg-[#a1d12c] hover:shadow-none ${isActive ? "translate-y-1px translate-x-1 bg-[#a1d12c] shadow-none transition-all ease-in-out" : ""}`
+            }
+            to="/"
+          >
+            <HouseIcon size={24} />
+            Home
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `font-bricolage-grotesque hover:translate-y-1px flex w-42 cursor-pointer gap-2 gap-x-4 border-2 border-black px-3 py-2 text-start font-semibold shadow-[4px_4px_0px_0px_rgba(34,34,34,1)] transition-all hover:translate-x-1 hover:bg-[#a1d12c] hover:shadow-none ${isActive ? "translate-y-1px translate-x-1 bg-[#a1d12c] shadow-none transition-all ease-in-out" : ""}`
+            }
+            to="/projects"
+          >
+            <FilesIcon size={24} />
+            Project
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `font-bricolage-grotesque hover:translate-y-1px flex w-42 cursor-pointer gap-2 gap-x-4 border-2 border-black px-3 py-2 text-start font-semibold shadow-[4px_4px_0px_0px_rgba(34,34,34,1)] transition-all hover:translate-x-1 hover:bg-[#a1d12c] hover:shadow-none ${isActive ? "translate-y-1px translate-x-1 bg-[#a1d12c] shadow-none transition-all ease-in-out" : ""}`
+            }
+            to="/skills"
+          >
+            <TerminalIcon size={24} />
+            Skills
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `font-bricolage-grotesque hover:translate-y-1px flex w-42 cursor-pointer gap-2 gap-x-4 border-2 border-black px-3 py-2 text-start font-semibold shadow-[4px_4px_0px_0px_rgba(34,34,34,1)] transition-all hover:translate-x-1 hover:bg-[#a1d12c] hover:shadow-none ${isActive ? "translate-y-1px translate-x-1 bg-[#a1d12c] shadow-none transition-all ease-in-out" : ""}`
+            }
+            to="/contact"
+          >
+            <EnvelopeIcon size={24} />
+            Contact
+          </NavLink>
         </div>
         <div className="mt-48 flex flex-col gap-y-1">
           <h4 className="font-bebas-neue text-xl font-semibold uppercase">
             find me on
           </h4>
           <ul className="flex w-fit justify-end gap-2">
-            <li className="cursor-point border-2 p-1 shadow-[2px_2px_0_0_rgba(34,34,34,1)]">
+            <li className="cursor-point hover:translate-y-1px border-2 p-1 shadow-[2px_2px_0_0_rgba(34,34,34,1)] transition-all ease-in-out hover:translate-x-1 hover:bg-[#a1d12c] hover:shadow-none">
               <a href="https://www.github.com/indraplrg" target="_blank">
                 <GithubLogoIcon size={24} weight="fill" />
               </a>
             </li>
-            <li className="cursor-point border-2 p-1 shadow-[2px_2px_0_0_rgba(34,34,34,1)]">
+            <li className="cursor-point hover:translate-y-1px border-2 p-1 shadow-[2px_2px_0_0_rgba(34,34,34,1)] transition-all ease-in-out hover:translate-x-1 hover:bg-[#a1d12c] hover:shadow-none">
               <a href="https://www.gitlab.com/indralolx" target="_blank">
                 <GitlabLogoIcon size={24} weight="fill" />
               </a>
             </li>
-            <li className="cursor-point border-2 p-1 shadow-[2px_2px_0_0_rgba(34,34,34,1)]">
+            <li className="cursor-point hover:translate-y-1px border-2 p-1 shadow-[2px_2px_0_0_rgba(34,34,34,1)] transition-all ease-in-out hover:translate-x-1 hover:bg-[#a1d12c] hover:shadow-none">
               <a href="https://instagram.com/asykarindra" target="_blank">
                 <LinkedinLogoIcon size={24} weight="fill" />
               </a>
             </li>
-            <li className="cursor-point border-2 p-1 shadow-[2px_2px_0_0_rgba(34,34,34,1)]">
+            <li className="cursor-point hover:translate-y-1px border-2 p-1 shadow-[2px_2px_0_0_rgba(34,34,34,1)] transition-all ease-in-out hover:translate-x-1 hover:bg-[#a1d12c] hover:shadow-none">
               <a href="https://instagram.com/asykarindra" target="_blank">
                 <InstagramLogoIcon size={24} weight="fill" />
               </a>
